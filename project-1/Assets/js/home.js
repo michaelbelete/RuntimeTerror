8c6b2414c4af0af202693bb27ae1a72c7fcd8ee1
BgitBB <61750923+BgitBB@users.noreply.github.com>
2021-02-15 23:34:55 +0300

    changed add button style in mybooks and wishlist to match post button
    and commented out the db opening code in home.js

--- project-1/Assets/js/home.js
+++ project-1/Assets/js/home.js
@@ -33,3 +33,56 @@ ClassicEditor
             $('#header').load('includes/header.html')
             $('#books').select2();
         });
+
+
+
+//abandoned __ to Mike
+// let DB;
+
+
+// // If we added sorting
+// // var isAlphaBeticalAsc = true;
+// // var isDateAsc = true;
+
+
+// let myBookDB = indexedDB.open('Bookaholics', 2);
+
+// myBookDB.onerror = function () {
+//     console.log('There was an error');
+// }
+
+// myBookDB.onsuccess = function () {
+//     // console.log('Database Ready');
+//     DB = myBookDB.result;
+//     // displayMyFeed();
+//     // displayMyBooks();
+//     // displayNewMembers();
+// }
+
+// myBookDB.onupgradeneeded = function (e) {
+//     let db = e.target.result;
+//     let objectStore = db.createObjectStore('myBooks', {
+//         keyPath: 'id',
+//         autoIncrement: true
+//     });
+//     objectStore.createIndex('bookTitle', 'bookTitle', {
+//         unique: false
+//     });
+//     objectStore.createIndex('edition', 'edition', {
+//         unique: false
+//     }); //might need us to add it in the My Books page
+//     // objectStore.createIndex('publicationDate', 'publicationDate', { unique: false }); //
+//     objectStore.createIndex('author', 'author', {
+//         unique: false
+//     });
+//     objectStore.createIndex('publisher', 'publisher', {
+//         unique: false
+//     });
+//     objectStore.createIndex('dateAdded', 'dateAdded', {
+//         unique: true
+//     });
+//     objectStore.createIndex('dateModified', 'dateModified', {
+//         unique: true
+//     });
+//     console.log('Database created.');
+// }
