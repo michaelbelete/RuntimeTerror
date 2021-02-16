ce82bc358b81017caf2f9509779d122b460af0fd
michael belete <it.michael.belete@gmail.com>
2021-02-16 17:21:36 +0300

    displayed post

--- project-1/Assets/js/db.js
+++ project-1/Assets/js/db.js
@@ -1,11 +1,11 @@
 const db = new Dexie('Bookacholics');
 
 db.version(3).stores({
-    users: "++useId, firstName, lastName, email, username, bio, hobbies, birthDate, currentCity, homeTown, education, registeredAt",
+    users: "++userId, firstName, lastName, email, &username, bio, hobbies, birthDate, currentCity, homeTown, education, profilePicture, registeredAt",
     books: "++bookId, title, author, edition, publisher, dateAdded, shortDesc, userId",
     wishlist: "++bookId, title, author, edition, publisher, dateAdded, shortDesc, whyWish, userId",
     posts: "++postId, bookId, postType, rating, comments, createdAt, updatedAt, userId",
-    comments: "++commentId, comment, userId, createdAt, updatedAt"
+    comments: "++commentId, postId, comment, userId, createdAt, updatedAt"
 });
 
 const newUser = {
@@ -19,6 +19,7 @@ const newUser = {
     currentCity: "Addis Ababa",
     homeTown: "Bahir Dar",
     education: "BSC",
+    profilePicture: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
     registeredAt: new Date(),
 }
 
@@ -29,14 +30,14 @@ const book = {
         publisher: "me",
         dateAdded: new Date(),
         shortDesc: "slorem aiusbasbjhbasjdbjasbbs",
-        userId: 1
+        userId: 1,
     }
     // db.users.put(newUser).then(function() {
     //     console.log("user created successfully")
     // }).catch((error) => console.log(error))
 
 // db.books.put(book)
-//     //creating a sample user
+//creating a sample user
 function loggedInUser() {
     return 1
 }
--- project-1/Assets/js/home.js
+++ project-1/Assets/js/home.js
@@ -15,6 +15,7 @@ const selectBooks = document.querySelector("#selectBooks")
 const postType = document.querySelector('#type')
 const post = document.querySelector("#postArea")
 const postMessage = document.querySelector("#postMessage")
+const feeds = document.querySelector("#feeds")
 
 document.addEventListener('DOMContentLoaded', function() {
     loadMyBooks()
@@ -67,12 +68,12 @@ $(function() {
         } else {
 
             const newPost = {
-                bookId: 2,
+                bookId: parseInt(selectBooks.value),
+                userId: loggedInUser(),
                 postType: postType.value, //rec, rev
                 rating: rating,
                 post: editor.getData(),
                 picture: "https://images.unsplash.com/photo-1549122728-f519709caa9c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80",
-                comments: [],
                 createdAt: new Date(),
                 updatedAt: ""
             }
@@ -86,6 +87,7 @@ $(function() {
                 postType.value = null
                 $rateYo.rateYo("rating", 0)
                 editor.setData(" ")
+                loadPosts()
             }).catch((error) => {
                 postMessage.classList.add("alert-danger")
                 postMessage.textContent = `Error ${ error }`
@@ -98,47 +100,94 @@ $(function() {
 
 async function loadPosts() {
     const posts = await db.table("posts").orderBy("postId").reverse().toArray()
-    posts.forEach(function(post) {
-        let book
-        console.log(post.bookId)
-        db.books.get(post.bookId, function(book) {
-            console.log(book)
-        })
-        console.log(book)
-        let htmlPost = `
-        <div class="card card-body mb-4 p-0">
-        <div class="row p-3">
-            <div class="col-2">
-                <a href="profile.html"><img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="profile" class="rounded-circle" width="70"
-                        height="70">
-                </a>
-            </div>
-            <div class="col-10 p-2">
-                <h5 class="text-primary pt-1 m-0">Michael Belete</h5>
-                <small class="text-muted p-0">${ post.createdAt }</small>
-            </div>
-        </div>
-        <div class="px-3 py-1">
-            <h4 class="font-weight-light text-muted title">Book title</h4>
-        </div>
-        <div class="px-3 text-justify w-100">
-            <p class="text-muted">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, illum architecto ea recusandae error, qui libero, hic facere repellat harum accusamus saepe eveniet possimus quidem... <a href="post.html?id=1">Read More</a>
-            </p>
-        </div>
+    posts.forEach(async function(post) {
 
-        <div class="rating-comment px-4 pb-3">
-            <div class="row">
-                <div class="col-md-8">
-                    <div id="post-rate2"></div>
+        let book = await db.books.where("bookId").equals(parseInt(post.bookId)).first()
+        let user = await db.users.where("userId").equals(post.userId).first()
+        let comments = await db.comments.where("postId").equals(post.postId).count()
+
+        let postNoHtmlTag = post.post.replace(/(<([^>]+)>)/gi, "")
+
+        if (post.picture == "") {
+            var strPost = `
+            <div class="card card-body mb-4 p-0">
+            <div class="row p-3">
+                <div class="col-2">
+                    <a href="profile.html"><img src="${user.profilePicture}" alt="profile" class="rounded-circle" width="70"
+                            height="70">
+                    </a>
                 </div>
-                <div class="col-md-4">
-                    <a href="post.html" class=" text-primary  float-right"><i class="fa fa-comment"></i>
-                        4 Comments</a>
+                <div class="col-10 p-2">
+                    <h5 class="text-primary pt-1 m-0">${ user.firstName } ${user.lastName}</h5>
+                    <small class="text-muted p-0">${ post.createdAt }</small>
+                </div>
+            </div>
+            <div class="px-3 py-1">
+                <h4 class="font-weight-light text-muted title">${ book.title }</h4>
+            </div>
+            <div class="px-3 text-justify w-100">
+                <p class="text-muted">
+                    ${ postNoHtmlTag.length > 100 ? postNoHtmlTag.substring(0,100) : postNoHtmlTag }    
+                </p>
+            </div>
+    
+            <div class="rating-comment px-4 pb-3">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div id="post-rate${post.postId}">${ post.rating }</div>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="post.html" class=" text-primary  float-right"><i class="fa fa-comment"></i>
+                            ${ comments } Comments</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>`
+        `
+        } else {
+            var strPost = `
+            <div class="card card-body mb-4 p-0">
+            <div class="row p-3">
+                <div class="col-2">
+                    <a href="profile.html?id=${user.userId}"><img src="${user.profilePicture}" alt="profile" class="rounded-circle" width="70"
+                            height="70">
+                    </a>
+                </div>
+                <div class="col-10 p-2">
+                    <h5 class="text-primary pt-1 m-0">${ user.firstName } ${user.lastName}</h5>
+                    <small class="text-muted p-0">${ post.createdAt }</small>
+                </div>
+            </div>
+            <div class="px-3 py-1">
+                <h4 class="font-weight-light text-muted title">${ book.title }</h4>
+            </div>
+
+            <div class="post-image">
+                <a href="post.html?id=${post.postId}"><img src="${ post.picture }" width="100%"></a>
+            </div>
+            <div class="px-3 text-justify w-100">
+                <p class="text-muted">
+                    ${ postNoHtmlTag.length > 100 ? postNoHtmlTag.substring(0,100) : postNoHtmlTag }    
+                </p>
+            </div>
+    
+            <div class="rating-comment px-4 pb-3">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div id="post-rate${post.postId }">${ post.rating }</div>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="post.html" class=" text-primary  float-right"><i class="fa fa-comment"></i>
+                            ${ comments } Comments</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        }
+
+        let htmlPost = new DOMParser().parseFromString(strPost, 'text/html')
+        feeds.appendChild(htmlPost.body.firstChild)
     })
 }
 $(document).ready(function() {
