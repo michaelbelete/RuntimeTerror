3173a7b27acc10ce503a769d33bb48029cbb40b5
BgitBB <61750923+BgitBB@users.noreply.github.com>
2021-02-16 05:18:18 +0300

    did the mybooks and wishlists js with dexie, added functionality to more info icon, and some more few edits

--- project-1/Assets/js/books_you_read.js
+++ project-1/Assets/js/books_you_read.js
@@ -1,4 +1,4 @@
-// need to change the active status of nav to MyBooks
+// need to change the active status of nav to mybooks
 $(document).ready(function () {
     $('#header').load('includes/header.html')
     $('#books').select2();
@@ -13,6 +13,7 @@ const titleInput = document.querySelector('.title');
 const editionInput = document.querySelector('.edition');
 const authorInput = document.querySelector('.author');
 const publisherInput = document.querySelector('.publisher');
+const shortDescInput = document.querySelector('.short-desc');
 
 const bookList = document.querySelector('.book-list');
 
@@ -22,60 +23,105 @@ const bookList = document.querySelector('.book-list');
 
 // Event Listeners
 addBook.addEventListener('submit', addNewBook);
-// removeBooks.addEventListener('click', removeAllBooks);
+// // removeBooks.addEventListener('click', removeAllBooks);
 // removeBook.addEventListener('click', removeBookF);
 bookList.addEventListener('click', removeBookF);
 searchFilter.addEventListener('keyup', filterBooks);
+bookList.addEventListener('click', displayMoreInfo);
+
+
+
+const db = new Dexie('Bookacholics');
+
+db.version(3).stores({
+    users: "++useId, firstName, lastName, email, &username, bio, hobbies, birthDate, currentCity, homeTown, education, registeredAt",
+    books: "++bookId, title, author, edition, publisher, dateAdded, shortDesc, userId",
+    wishlist: "++bookId, title, author, edition, publisher, dateAdded, whyWish, userId",
+    posts: "++postId, bookId, postType, rating, comments, createdAt, updatedAt, userId",
+    comments: "++commentId, comment, userId, createdAt, updatedAt"
+});
+
+db.open().then(displayMyBooks());
+
+const newUser = {
+    firstName: "michael",
+    lastName: "belete",
+    email: "it.michael.belete@gmail.com",
+    username: "@abc",
+    bio: "some bio...",
+    hobbies: "programming",
+    birthDate: Date(),
+    currentCity: "Addis Ababa",
+    homeTown: "Bahir Dar",
+    education: "BSC",
+    registeredAt: Date(),
+}
+
+db.users.put(newUser).then(function() {
+    console.log("user created succesfully")
+}).catch((error) => console.log(error))
+
+
+
+
+
+
+
+
+
 
 
 // --------------
-let DB;
+// let DB;
+let userID = sessionStorage.getItem('userId');
+
+// // If we added sorting
+// // var isAlphaBeticalAsc = true;
+// // var isDateAsc = true;
 
 
-// If we added sorting
-// var isAlphaBeticalAsc = true;
-// var isDateAsc = true;
+// let myBookDB = indexedDB.open('Bookaholics', 2);
 
+// myBookDB.onerror = function () {
+//     console.log('There was an error');
+// }
 
-let myBookDB = indexedDB.open('Bookaholics', 2);
+// myBookDB.onsuccess = function () {
+//     // console.log('Database Ready');
+//     DB = myBookDB.result;
+//     displayMyBooks();
+// }
 
-myBookDB.onerror = function () {
-    console.log('There was an error');
-}
-
-myBookDB.onsuccess = function () {
-    // console.log('Database Ready');
-    DB = myBookDB.result;
-    displayMyBooks();
-}
-
-myBookDB.onupgradeneeded = function (e) {
-    let db = e.target.result;
-    let objectStore = db.createObjectStore('myBooks', {
-        keyPath: 'id',
-        autoIncrement: true
-    });
-    objectStore.createIndex('bookTitle', 'bookTitle', {
-        unique: false
-    });
-    objectStore.createIndex('edition', 'edition', {
-        unique: false
-    }); //might need us to add it in the My Books page
-    // objectStore.createIndex('publicationDate', 'publicationDate', { unique: false }); //
-    objectStore.createIndex('author', 'author', {
-        unique: false
-    });
-    objectStore.createIndex('publisher', 'publisher', {
-        unique: false
-    });
-    objectStore.createIndex('dateAdded', 'dateAdded', {
-        unique: true
-    });
-    objectStore.createIndex('dateModified', 'dateModified', {
-        unique: true
-    });
-    console.log('Database created.');
-}
+// myBookDB.onupgradeneeded = function (e) {
+//     let db = e.target.result;
+//     let objectStore = db.createObjectStore('mybooks', {
+//         keyPath: 'bookid',
+//         autoIncrement: true
+//     });
+//     objectStore.createIndex('userId', 'userId', {
+//         unique: false
+//     });
+//     objectStore.createIndex('bookTitle', 'bookTitle', {
+//         unique: false
+//     });
+//     objectStore.createIndex('edition', 'edition', {
+//         unique: false
+//     }); //might need us to add it in the My Books page
+//     // objectStore.createIndex('publicationDate', 'publicationDate', { unique: false }); //
+//     objectStore.createIndex('author', 'author', {
+//         unique: false
+//     });
+//     objectStore.createIndex('publisher', 'publisher', {
+//         unique: false
+//     });
+//     objectStore.createIndex('dateAdded', 'dateAdded', {
+//         unique: true
+//     });
+//     objectStore.createIndex('shortDesc', 'shortDesc', {
+//         unique: false
+//     });
+//     console.log('Database created.');
+// }
 
 
 
@@ -84,38 +130,29 @@ myBookDB.onupgradeneeded = function (e) {
 function addNewBook(e) {
     e.preventDefault();
 
-    if (!titleInput.value || !authorInput.value || !editionInput.value) {
-        alert("nah"); //will change it
-        return;
-    }
+    // if (!titleInput.value || !authorInput.value || !editionInput.value) {
+    //     alert("nah"); //will change it
+    //     return;
+    // }
 
     console.log("here");
     let newBook = {
-        bookTitle: titleInput.value,
-        edition: editionInput.value,
+        title: titleInput.value,
         author: authorInput.value,
+        edition: editionInput.value,
         publisher: publisherInput.value,
         dateAdded: Date(),
-        dateModified: Date()
+        shortDesc: shortDescInput.value,
+        // userId: userID
+        userId: 1
     }
 
-    let transaction = DB.transaction(['myBooks'], 'readwrite');
-    let objectStore = transaction.objectStore('myBooks');
-    
-    console.log(newBook)
-    let request = objectStore.add(newBook);
-
-    request.onsuccess = () => {
+    db.books.put(newBook).then(function() {
+        console.log("book created succesfully")
         addBook.reset();
-        // will potentially do other things here like closing the pop-up and maybe some animation saying 'congragulations on you read' or sth along that line
-    }
-    transaction.oncomplete = () => {
-        console.log('Book added.');
         displayMyBooks();
-    }
-    transaction.onerror = () => {
-        console.log('There was an error, try again!');
-    }
+    }).catch((error) => console.log(error))
+    
 
 }
 
@@ -125,22 +162,20 @@ function displayMyBooks() {
         bookList.removeChild(bookList.firstElementChild);
     }
 
-    let objectStore = DB.transaction('myBooks').objectStore('myBooks');
-    objectStore.openCursor().onsuccess = function (e) {
-        let cursor = e.target.result;
-        if (cursor) {
-            let div = document.createElement('div');
-            div.className = "row py-2 px-4 w-100 book";
-            div.setAttribute('my-book-id', cursor.value.id); // will be useful for deleting [through .delete()]
 
-            div.innerHTML = `<div class="col-lg-7 d-flex">
+    db.books.each(book => {
+        let div = document.createElement('div');
+        div.className = "row py-2 px-4 w-100 book";
+        div.setAttribute('my-book-id', book.bookId); // will be useful for deleting [through .delete()]
+
+        div.innerHTML = `<div class="col-lg-7 d-flex">
               <div class="col-1 p-0 mr-3">
                 <i class="fa fa-book fa-3x text-secondary"></i>
               </div>
               <div class="col-10 p">
-                <h6 class="py-0"><b class="text-primary">${cursor.value.bookTitle} </b><i class="fa fa-caret-right"
-                    aria-hidden="true"></i><span> ${cursor.value.author}</span></h6>
-                <p class="text-muted py-0">${cursor.value.publisher}</p>
+                <h6 class="py-0"><b class="text-primary">${book.title} </b><i class="fa fa-caret-right"
+                    aria-hidden="true"></i><span> ${book.author}</span></h6>
+                <p class="text-muted py-0">${book.publisher}</p>
               </div>
             </div>
             <div class="col-lg-2">
@@ -151,40 +186,49 @@ function displayMyBooks() {
             </div>
             <div class="col-lg-3">
               <h4 class="px-2 pt-2">
-                <a href="modifyBook.html?id=${cursor.value.id}"><i class="fas fa-edit text-secondary"></i></a>
+                <a href="modifyBook.html?id=${book.bookId}"><i class="fas fa-edit text-secondary"></i></a>
                 <i class="fas fa-trash text-danger remove-book"></i>
-                <a href=""><i class="fas fa-ellipsis-h bg-dark border rounded-pill text-white"></i></a>
+                <i class="fas fa-ellipsis-h bg-dark border rounded-pill text-white more-info"></i>
               </h4>
+            </div>
+            <div class="d-none">
+                <div class="col-lg-7 d-flex">
+                    <div class="col-1 invisible mr-3">Hey</div>
+                    <div class="col-10">
+                        <p>
+                            ${book.shortDesc}
+                        </p>
+                    </div>
+                    
+                </div>
+                <div class="col-lg-5 invisible">
+                    "Hello"
+                </div>
+                
             </div>`
 
 
-            bookList.prepend(div); //may wanna prepend
-
-            cursor.continue();
-        }
-        
-        // console.log(bookList.firstElementChild);
-        if (!bookList.firstElementChild){
-            let d = document.createElement('div');
-            let p = document.createElement('p');
-            p.textContent = "Seems like you have not added any books in your wishlist. Click the Add New Book button above to start adding.";
-            p.className = "text-center";
-            d.appendChild(p);
-            bookList.appendChild(d);
-        }
-    }
-    
-
-
+        bookList.prepend(div); //may wanna prepend
+        // console.log("not");
+    })
+    // .then(console.log("HERE"))
+    // .catch(console.log("there"))
+    // // console.log(bookList.firstElementChild);
+    // if (!bookList.firstElementChild) {
+    //     let d = document.createElement('div');
+    //     let p = document.createElement('p');
+    //     p.textContent = "Seems like you have not added any books in your read-book list. Click the Add New Book button above to start adding.";
+    //     p.className = "text-center";
+    //     d.appendChild(p);
+    //     bookList.appendChild(d);
+    // }
 }
 
 
 
 
-
 function removeAllBooks() {
-    let myBooks = DB.transaction("myBooks", "readwrite").objectStore("myBooks");
-    myBooks.clear();
+    db.books.clear()
     displayMyBooks();
 }
 
@@ -192,38 +236,21 @@ function removeAllBooks() {
 
 
 
-// function removeBookF(e) {
-//     console.log('here')
-//     if (e.target.classList.contains('remove-book')) {
-//         if (confirm('Are You Sure about that ?')) {
-//             let bookID = Number(e.target.parentElement.parentElement.parentElement.getAttribute('my-book-id'));
-//             let transaction = DB.transaction('myBooks', 'readwrite');
-//             let objectStore = transaction.objectStore('myBooks');
-//             objectStore.delete(bookID);
-//             console.log(bookID)
-//             transaction.oncomplete = () => {
-//                 e.target.parentElement.parentElement.parentElement.remove();
-//             }
-//         }
-//     }
-// }
-
 
 
 
 // if we delegated the event from the ul //will need the immediate parent of the fa-remove i to have .remove-book class
 // bookList.addEventListener('click', removeBookF);
 function removeBookF(e) {
+    // console.log('here');
     if (e.target.classList.contains('remove-book')) {
         if (confirm('Are You Sure about that ?')) {
             let bookID = Number(e.target.parentElement.parentElement.parentElement.getAttribute('my-book-id'));
-            let transaction = DB.transaction('myBooks', 'readwrite');
-            let objectStore = transaction.objectStore('myBooks');
-            objectStore.delete(bookID);
-            // console.log(bookID)
-            transaction.oncomplete = () => {
-                e.target.parentElement.parentElement.parentElement.remove();
-            }
+            db.books.delete(bookID);
+            console.log(bookID)
+            e.target.parentElement.parentElement.parentElement.remove();
+            
+            
         }
     }
 }
@@ -236,6 +263,12 @@ function removeBookF(e) {
 function filterBooks() {
     var noResult = true;
     var noMatch = document.querySelector(".no-match");
+
+    // if (!bookList.firstChild.className.includes('book')){
+    //     bookList.firstChild.textContent += "\n You can't search an empty book list.";
+    //     return;
+    // }
+
     document.querySelectorAll('.book').forEach(el => {
         if (el.textContent.toLocaleLowerCase().includes(searchFilter.value.toLocaleLowerCase())) { //will need improvement (to only check text that is relevant)
             el.style.display = "flex";
@@ -254,4 +287,18 @@ function filterBooks() {
     }
 
 
+}
+
+let isInvisbile = true;
+function displayMoreInfo(e){
+    // console.log(isInvisbile);
+    if (e.target.classList.contains('more-info')) {
+        // console.log(isInvisbile);
+        e.target.parentElement.parentElement.nextElementSibling.setAttribute("style", `display:${isInvisbile ? "flex" : "none"} !important`);
+        // console.log(e.target.parentElement.parentElement.nextElementSibling);
+        // console.log(isInvisbile);
+        if(isInvisbile) isInvisbile = false;
+        else isInvisbile = true;
+        // console.log(isInvisbile);
+    }
 }
--- project-1/Assets/js/wishlist.js
+++ project-1/Assets/js/wishlist.js
@@ -13,6 +13,7 @@ const titleInput = document.querySelector('.title');
 const editionInput = document.querySelector('.edition');
 const authorInput = document.querySelector('.author');
 const publisherInput = document.querySelector('.publisher');
+const whyWishInput = document.querySelector('.why-wish');
 
 const bookList = document.querySelector('.book-list');
 
@@ -22,60 +23,105 @@ const bookList = document.querySelector('.book-list');
 
 // Event Listeners
 addBook.addEventListener('submit', addNewBook);
-// removeBooks.addEventListener('click', removeAllBooks);
+// // removeBooks.addEventListener('click', removeAllBooks);
 // removeBook.addEventListener('click', removeBookF);
 bookList.addEventListener('click', removeBookF);
 searchFilter.addEventListener('keyup', filterBooks);
 
 
+
+
+const db = new Dexie('Bookacholics');
+
+db.version(3).stores({
+    users: "++useId, firstName, lastName, email, &username, bio, hobbies, birthDate, currentCity, homeTown, education, registeredAt",
+    books: "++bookId, title, author, edition, publisher, dateAdded, shortDesc, userId",
+    wishlist: "++bookId, title, author, edition, publisher, dateAdded, whyWish, userId",
+    posts: "++postId, bookId, postType, rating, comments, createdAt, updatedAt, userId",
+    comments: "++commentId, comment, userId, createdAt, updatedAt"
+});
+
+db.open().then(displayMyBooks());
+
+const newUser = {
+    firstName: "michael",
+    lastName: "belete",
+    email: "it.michael.belete@gmail.com",
+    username: "@abc",
+    bio: "some bio...",
+    hobbies: "programming",
+    birthDate: Date(),
+    currentCity: "Addis Ababa",
+    homeTown: "Bahir Dar",
+    education: "BSC",
+    registeredAt: Date(),
+}
+
+db.users.put(newUser).then(function() {
+    console.log("user created succesfully")
+}).catch((error) => console.log(error))
+
+
+
+
+
+
+
+
+
+
+
 // --------------
-let DB;
+// let DB;
+let userID = sessionStorage.getItem('userId');
+
+// // If we added sorting
+// // var isAlphaBeticalAsc = true;
+// // var isDateAsc = true;
 
 
-// If we added sorting
-// var isAlphaBeticalAsc = true;
-// var isDateAsc = true;
+// let myBookDB = indexedDB.open('Bookaholics', 2);
 
+// myBookDB.onerror = function () {
+//     console.log('There was an error');
+// }
 
-let myBookDB = indexedDB.open('Bookaholics', 2);
+// myBookDB.onsuccess = function () {
+//     // console.log('Database Ready');
+//     DB = myBookDB.result;
+//     displayMyBooks();
+// }
 
-myBookDB.onerror = function () {
-    console.log('There was an error');
-}
-
-myBookDB.onsuccess = function () {
-    // console.log('Database Ready');
-    DB = myBookDB.result;
-    displayMyBooks();
-}
-
-myBookDB.onupgradeneeded = function (e) {
-    let db = e.target.result;
-    let objectStore = db.createObjectStore('myWishlist', {
-        keyPath: 'id',
-        autoIncrement: true
-    });
-    objectStore.createIndex('bookTitle', 'bookTitle', {
-        unique: false
-    });
-    objectStore.createIndex('edition', 'edition', {
-        unique: false
-    }); //might need us to add it in the My Books page
-    // objectStore.createIndex('publicationDate', 'publicationDate', { unique: false }); //
-    objectStore.createIndex('author', 'author', {
-        unique: false
-    });
-    objectStore.createIndex('publisher', 'publisher', {
-        unique: false
-    });
-    objectStore.createIndex('dateAdded', 'dateAdded', {
-        unique: true
-    });
-    objectStore.createIndex('dateModified', 'dateModified', {
-        unique: true
-    });
-    console.log('Database created.');
-}
+// myBookDB.onupgradeneeded = function (e) {
+//     let db = e.target.result;
+//     let objectStore = db.createObjectStore('myWishlist', {
+//         keyPath: 'bookid',
+//         autoIncrement: true
+//     });
+//     objectStore.createIndex('userId', 'userId', {
+//         unique: false
+//     });
+//     objectStore.createIndex('bookTitle', 'bookTitle', {
+//         unique: false
+//     });
+//     objectStore.createIndex('edition', 'edition', {
+//         unique: false
+//     }); //might need us to add it in the My Books page
+//     // objectStore.createIndex('publicationDate', 'publicationDate', { unique: false }); //
+//     objectStore.createIndex('author', 'author', {
+//         unique: false
+//     });
+//     objectStore.createIndex('publisher', 'publisher', {
+//         unique: false
+//     });
+//     objectStore.createIndex('dateAdded', 'dateAdded', {
+//         unique: true
+//     });
+//     objectStore.createIndex('shortDesc', 'shortDesc', {
+//         unique: false
+//     });
+//     console.log('Database created.');
+// }
 
 
 
@@ -84,38 +130,29 @@ myBookDB.onupgradeneeded = function (e) {
 function addNewBook(e) {
     e.preventDefault();
 
-    if (!titleInput.value || !authorInput.value || !editionInput.value) {
-        alert("nah"); //will change it
-        return;
-    }
+    // if (!titleInput.value || !authorInput.value || !editionInput.value) {
+    //     alert("nah"); //will change it
+    //     return;
+    // }
 
     console.log("here");
     let newBook = {
-        bookTitle: titleInput.value,
-        edition: editionInput.value,
+        title: titleInput.value,
         author: authorInput.value,
+        edition: editionInput.value,
         publisher: publisherInput.value,
         dateAdded: Date(),
-        dateModified: Date()
+        whyWish: whyWishInput.value,
+        // userId: userID
+        userId: 1
     }
 
-    let transaction = DB.transaction(['myWishlist'], 'readwrite');
-    let objectStore = transaction.objectStore('myWishlist');
-    
-    console.log(newBook)
-    let request = objectStore.add(newBook);
-
-    request.onsuccess = () => {
+    db.wishlist.put(newBook).then(function() {
+        console.log("book created succesfully")
         addBook.reset();
-        // will potentially do other things here like closing the pop-up and maybe some animation saying 'congragulations on you read' or sth along that line
-    }
-    transaction.oncomplete = () => {
-        console.log('Book added.');
         displayMyBooks();
-    }
-    transaction.onerror = () => {
-        console.log('There was an error, try again!');
-    }
+    }).catch((error) => console.log(error))
+    
 
 }
 
@@ -125,22 +162,20 @@ function displayMyBooks() {
         bookList.removeChild(bookList.firstElementChild);
     }
 
-    let objectStore = DB.transaction('myWishlist').objectStore('myWishlist');
-    objectStore.openCursor().onsuccess = function (e) {
-        let cursor = e.target.result;
-        if (cursor) {
-            let div = document.createElement('div');
-            div.className = "row py-2 px-4 w-100 book";
-            div.setAttribute('my-book-id', cursor.value.id); // will be useful for deleting [through .delete()]
 
-            div.innerHTML = `<div class="col-lg-7 d-flex">
+    db.wishlist.each(book => {
+        let div = document.createElement('div');
+        div.className = "row py-2 px-4 w-100 book";
+        div.setAttribute('my-book-id', book.bookId); // will be useful for deleting [through .delete()]
+
+        div.innerHTML = `<div class="col-lg-7 d-flex">
               <div class="col-1 p-0 mr-3">
                 <i class="fa fa-book fa-3x text-secondary"></i>
               </div>
               <div class="col-10 p">
-                <h6 class="py-0"><b class="text-primary">${cursor.value.bookTitle} </b><i class="fa fa-caret-right"
-                    aria-hidden="true"></i><span> ${cursor.value.author}</span></h6>
-                <p class="text-muted py-0">${cursor.value.publisher}</p>
+                <h6 class="py-0"><b class="text-primary">${book.title} </b><i class="fa fa-caret-right"
+                    aria-hidden="true"></i><span> ${book.author}</span></h6>
+                <p class="text-muted py-0">${book.publisher}</p>
               </div>
             </div>
             <div class="col-lg-2">
@@ -151,40 +186,34 @@ function displayMyBooks() {
             </div>
             <div class="col-lg-3">
               <h4 class="px-2 pt-2">
-                <a href="modifyBook.html?id=${cursor.value.id}"><i class="fas fa-edit text-secondary"></i></a>
+                <a href="modifyBook.html?id=${book.bookId}"><i class="fas fa-edit text-secondary"></i></a>
                 <i class="fas fa-trash text-danger remove-book"></i>
                 <a href=""><i class="fas fa-ellipsis-h bg-dark border rounded-pill text-white"></i></a>
               </h4>
             </div>`
 
 
-            bookList.prepend(div); //may wanna prepend
-
-            cursor.continue();
-        }
-        // console.log(bookList.firstElementChild);
-        if (!bookList.firstElementChild){
-            let d = document.createElement('div');
-            let p = document.createElement('p');
-            p.textContent = "Seems like you have not added any books in your wishlist. Click the Add New Book button above to start adding.";
-            p.className = "text-center";
-            d.appendChild(p);
-            bookList.appendChild(d);
-    }
-    }
-
-    
-
-
+        bookList.prepend(div); //may wanna prepend
+        // console.log("not");
+    })
+    // .then(console.log("HERE"))
+    // .catch(console.log("there"))
+    // // console.log(bookList.firstElementChild);
+    // if (!bookList.firstElementChild) {
+    //     let d = document.createElement('div');
+    //     let p = document.createElement('p');
+    //     p.textContent = "Seems like you have not added any books in your wishlist. Click the Add New Book button above to start adding.";
+    //     p.className = "text-center";
+    //     d.appendChild(p);
+    //     bookList.appendChild(d);
+    // }
 }
 
 
 
 
-
 function removeAllBooks() {
-    let myWishlist = DB.transaction("myWishlist", "readwrite").objectStore("myWishlist");
-    myWishlist.clear();
+    db.wishlist.clear()
     displayMyBooks();
 }
 
@@ -192,21 +221,21 @@ function removeAllBooks() {
 
 
 
-// function removeBookF(e) {
-//     console.log('here')
-//     if (e.target.classList.contains('remove-book')) {
-//         if (confirm('Are You Sure about that ?')) {
-//             let bookID = Number(e.target.parentElement.parentElement.parentElement.getAttribute('my-book-id'));
-//             let transaction = DB.transaction('myWishlist', 'readwrite');
-//             let objectStore = transaction.objectStore('myWishlist');
-//             objectStore.delete(bookID);
-//             console.log(bookID)
-//             transaction.oncomplete = () => {
-//                 e.target.parentElement.parentElement.parentElement.remove();
-//             }
-//         }
-//     }
-// }
+// // function removeBookF(e) {
+// //     console.log('here')
+// //     if (e.target.classList.contains('remove-book')) {
+// //         if (confirm('Are You Sure about that ?')) {
+// //             let bookID = Number(e.target.parentElement.parentElement.parentElement.getAttribute('my-book-id'));
+// //             let transaction = DB.transaction('myWishlist', 'readwrite');
+// //             let objectStore = transaction.objectStore('myWishlist');
+// //             objectStore.delete(bookID);
+// //             console.log(bookID)
+// //             transaction.oncomplete = () => {
+// //                 e.target.parentElement.parentElement.parentElement.remove();
+// //             }
+// //         }
+// //     }
+// // }
 
 
 
@@ -214,16 +243,15 @@ function removeAllBooks() {
 // if we delegated the event from the ul //will need the immediate parent of the fa-remove i to have .remove-book class
 // bookList.addEventListener('click', removeBookF);
 function removeBookF(e) {
+    // console.log('here');
     if (e.target.classList.contains('remove-book')) {
         if (confirm('Are You Sure about that ?')) {
             let bookID = Number(e.target.parentElement.parentElement.parentElement.getAttribute('my-book-id'));
-            let transaction = DB.transaction('myWishlist', 'readwrite');
-            let objectStore = transaction.objectStore('myWishlist');
-            objectStore.delete(bookID);
-            // console.log(bookID)
-            transaction.oncomplete = () => {
-                e.target.parentElement.parentElement.parentElement.remove();
-            }
+            db.wishlist.delete(bookID);
+            console.log(bookID)
+            e.target.parentElement.parentElement.parentElement.remove();
+            
+            
         }
     }
 }
