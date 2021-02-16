5a587ac5f8a19a829b498fbbb0e7720ca188fdb9
michael belete <it.michael.belete@gmail.com>
2021-02-17 02:05:15 +0300

    finished fetching post and creating comment

--- project-1/Assets/js/dist/post.dev.js
+++ project-1/Assets/js/dist/post.dev.js
@@ -5,10 +5,14 @@ $(document).ready(function () {
 });
 var urlParams = new URLSearchParams(window.location.search);
 var id = parseInt(urlParams.get('id'));
+var specificPost = document.querySelector('#specificPost');
+var comment = document.querySelector('#comment');
+var commentBtn = document.querySelector("#commentBtn");
 document.addEventListener('DOMContentLoaded', function () {
   checkPostId();
   loadSpecificPost();
 });
+commentBtn.addEventListener("click", addComment);
 
 function checkPostId() {
   if (id) {
@@ -18,8 +22,20 @@ function checkPostId() {
   }
 }
 
+function generateStar(id, rating) {
+  $(function () {
+    $("#post-rate".concat(id)).rateYo({
+      rating: rating,
+      starWidth: "20px",
+      ratedFill: "#07a8e2",
+      halfStar: true,
+      readOnly: true
+    });
+  });
+}
+
 function loadSpecificPost() {
-  var post, book, user, commentData, commentCount, comments;
+  var post, book, user, commentData, commentCount, comments, strPost, htmlPost;
   return regeneratorRuntime.async(function loadSpecificPost$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
@@ -57,9 +73,59 @@ function loadSpecificPost() {
         case 17:
           comments = _context.sent;
 
-        case 18:
+          if (post.picture == "") {
+            strPost = "\n    <div class=\"card card-body mb-4 p-0\">\n    <div class=\"row p-3\">\n        <div class=\"col-2\">\n            <a href=\"profile.html\"><img src=\"".concat(user.profilePicture, "\" alt=\"profile\" class=\"rounded-circle\" width=\"70\"\n                    height=\"70\">\n            </a>\n        </div>\n        <div class=\"col-10 p-2\">\n            <h5 class=\"text-primary pt-1 m-0\">").concat(user.firstName, " ").concat(user.lastName, "</h5>\n            <small class=\"text-muted p-0\">").concat(post.createdAt, "</small>\n        </div>\n    </div>\n    <div class=\"px-3 py-1\">\n        <h4 class=\"font-weight-light text-muted title\">").concat(book.title, "</h4>\n    </div>\n    <div class=\"px-3 text-justify w-100\">\n        <p class=\"text-muted\">\n            ").concat(post.post, "    \n        </p>\n    </div>\n\n    <div class=\"rating-comment px-4 pb-3\">\n        <div class=\"row\">\n            <div class=\"col-md-8\">\n                <div id=\"post-rate").concat(post.postId, "\">").concat(post.rating, "</div>\n            </div>\n            <div class=\"col-md-4\">\n                <a href=\"post.html\" class=\" text-primary  float-right\"><i class=\"fa fa-comment\"></i>\n                    ").concat(commentCount, " Comments</a>\n            </div>\n        </div>\n    </div>\n</div>\n");
+          } else {
+            strPost = "\n    <div class=\"card card-body mb-4 p-0\">\n    <div class=\"row p-3\">\n        <div class=\"col-2\">\n            <a href=\"profile.html?id=".concat(user.userId, "\"><img src=\"").concat(user.profilePicture, "\" alt=\"profile\" class=\"rounded-circle\" width=\"70\"\n                    height=\"70\">\n            </a>\n        </div>\n        <div class=\"col-10 p-2\">\n            <h5 class=\"text-primary pt-1 m-0\">").concat(user.firstName, " ").concat(user.lastName, " <small class=\"text-muted\"><i class=\"fa fa-angle-right\"></i> ").concat(post.postType == "recommendation" ? "Recommended" : "Reviewed", " ").concat(book.title, " By <b>").concat(book.author, "</b></small></h5>\n            <small class=\"text-muted p-0\">").concat(post.createdAt, "</small>\n        </div>\n    </div>\n\n    <div class=\"post-image\">\n        <a href=\"post.html?id=").concat(post.postId, "\"><img src=\"").concat(post.picture, "\" width=\"100%\"></a>\n    </div>\n    <div class=\"px-3 border-left border-bottom border-right text-justify w-100\">\n        <p class=\"text-muted border-left border-right border-bottom p-3\">\n            ").concat(post.post, "   \n        </p>\n    </div>\n\n    <div class=\"rating-comment px-4 py-2 pb-3\">\n        <div class=\"row\">\n            <div class=\"col-md-8\">\n                <div id=\"post-rate").concat(post.postId, "\">").concat(post.rating, "</div>\n            </div>\n            <div class=\"col-md-4\">\n                <a href=\"post.html\" class=\" text-primary  float-right\"><i class=\"fa fa-comment\"></i>\n                    ").concat(commentCount, " Comments</a>\n            </div>\n        </div>\n    </div>\n</div>\n");
+          }
+
+          htmlPost = new DOMParser().parseFromString(strPost, 'text/html');
+          specificPost.appendChild(htmlPost.body.firstChild);
+          generateStar(post.postId, post.rating); // comments.forEach(comment => {
+          // });
+
+        case 22:
         case "end":
           return _context.stop();
+      }
+    }
+  });
+}
+
+function addComment() {
+  var newComment;
+  return regeneratorRuntime.async(function addComment$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          if (!(comment.value === "")) {
+            _context2.next = 4;
+            break;
+          }
+
+          console.log("empty");
+          _context2.next = 7;
+          break;
+
+        case 4:
+          newComment = {
+            userId: loggedInUser(),
+            postId: id,
+            comment: comment.value,
+            createdAt: new Date(),
+            updatedAt: ""
+          };
+          _context2.next = 7;
+          return regeneratorRuntime.awrap(db.comments.add(newComment).then(function (result) {
+            console.log(result);
+            console.log("successful");
+          })["catch"](function (error) {
+            return console.log(error);
+          }));
+
+        case 7:
+        case "end":
+          return _context2.stop();
       }
     }
   });
--- project-1/Assets/js/post.js
+++ project-1/Assets/js/post.js
@@ -6,10 +6,18 @@ const urlParams = new URLSearchParams(window.location.search)
 
 const id = parseInt(urlParams.get('id'))
 
+const specificPost = document.querySelector('#specificPost')
+
+const comment = document.querySelector('#comment')
+const commentBtn = document.querySelector("#commentBtn")
+
+
 document.addEventListener('DOMContentLoaded', function() {
     checkPostId()
     loadSpecificPost()
 })
+
+commentBtn.addEventListener("click", addComment)
 
 
 function checkPostId() {
@@ -20,6 +28,17 @@ function checkPostId() {
     }
 }
 
+function generateStar(id, rating) {
+    $(function() {
+        $(`#post-rate${id}`).rateYo({
+            rating: rating,
+            starWidth: "20px",
+            ratedFill: "#07a8e2",
+            halfStar: true,
+            readOnly: true,
+        })
+    })
+}
 async function loadSpecificPost() {
     const post = await db.posts.where({
         postId: id
@@ -30,5 +49,105 @@ async function loadSpecificPost() {
     const commentCount = await commentData.count()
     const comments = await commentData.toArray()
 
+    if (post.picture == "") {
+        var strPost = `
+    <div class="card card-body mb-4 p-0">
+    <div class="row p-3">
+        <div class="col-2">
+            <a href="profile.html"><img src="${user.profilePicture}" alt="profile" class="rounded-circle" width="70"
+                    height="70">
+            </a>
+        </div>
+        <div class="col-10 p-2">
+            <h5 class="text-primary pt-1 m-0">${ user.firstName } ${user.lastName}</h5>
+            <small class="text-muted p-0">${ post.createdAt }</small>
+        </div>
+    </div>
+    <div class="px-3 py-1">
+        <h4 class="font-weight-light text-muted title">${ book.title }</h4>
+    </div>
+    <div class="px-3 text-justify w-100">
+        <p class="text-muted">
+            ${ post.post }    
+        </p>
+    </div>
 
+    <div class="rating-comment px-4 pb-3">
+        <div class="row">
+            <div class="col-md-8">
+                <div id="post-rate${post.postId}">${ post.rating }</div>
+            </div>
+            <div class="col-md-4">
+                <a href="post.html" class=" text-primary  float-right"><i class="fa fa-comment"></i>
+                    ${ commentCount } Comments</a>
+            </div>
+        </div>
+    </div>
+</div>
+`
+    } else {
+        var strPost = `
+    <div class="card card-body mb-4 p-0">
+    <div class="row p-3">
+        <div class="col-2">
+            <a href="profile.html?id=${user.userId}"><img src="${user.profilePicture}" alt="profile" class="rounded-circle" width="70"
+                    height="70">
+            </a>
+        </div>
+        <div class="col-10 p-2">
+            <h5 class="text-primary pt-1 m-0">${ user.firstName } ${user.lastName} <small class="text-muted"><i class="fa fa-angle-right"></i> ${ post.postType == "recommendation" ? "Recommended": "Reviewed" } ${ book.title } By <b>${ book.author }</b></small></h5>
+            <small class="text-muted p-0">${ post.createdAt }</small>
+        </div>
+    </div>
+
+    <div class="post-image">
+        <a href="post.html?id=${post.postId}"><img src="${ post.picture }" width="100%"></a>
+    </div>
+    <div class="px-3 border-left border-bottom border-right text-justify w-100">
+        <p class="text-muted border-left border-right border-bottom p-3">
+            ${post.post}   
+        </p>
+    </div>
+
+    <div class="rating-comment px-4 py-2 pb-3">
+        <div class="row">
+            <div class="col-md-8">
+                <div id="post-rate${post.postId }">${ post.rating }</div>
+            </div>
+            <div class="col-md-4">
+                <a href="post.html" class=" text-primary  float-right"><i class="fa fa-comment"></i>
+                    ${ commentCount } Comments</a>
+            </div>
+        </div>
+    </div>
+</div>
+`
+    }
+
+    let htmlPost = new DOMParser().parseFromString(strPost, 'text/html')
+    specificPost.appendChild(htmlPost.body.firstChild)
+    generateStar(post.postId, post.rating)
+
+    // comments.forEach(comment => {
+
+    // });
+}
+
+async function addComment() {
+    if (comment.value === "") {
+        console.log("empty")
+    } else {
+        const newComment = {
+            userId: loggedInUser(),
+            postId: id,
+            comment: comment.value,
+            createdAt: new Date(),
+            updatedAt: ""
+        }
+
+        await db.comments.add(newComment).then((result) => {
+            console.log(result)
+            console.log("successful")
+        }).catch((error) => console.log(error))
+    }
 }
