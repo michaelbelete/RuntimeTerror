1bde2ababc72c94e0ce43702e42de8f0f3502907
michael belete <it.michael.belete@gmail.com>
2021-03-04 05:00:20 +0300

    added profile

--- project-1/Assets/js/profile.js
+++ project-1/Assets/js/profile.js
@@ -1,11 +1,38 @@
+import {
+  editAboutMe,
+  editButtonAboutMe,
+  saveHobbies,
+  editHobbies,
+} from "./methods.js";
+
 const fullName = document.querySelector("#full_name");
 const userName = document.querySelector("#user_name");
 const email = document.querySelector("#email");
 const bDate = document.querySelector("#b_date");
 const sex = document.querySelector("#sex");
 const pp_container = document.querySelector("#pp_container");
-
+const nationality = document.querySelector("#nationality");
+const homeTown = document.querySelector("#home_town");
+const currentCity = document.querySelector("#current_city");
+const education = document.querySelector("#education");
+const nationalityEdit = document.querySelector("#nationality_edit");
+const homeTownEdit = document.querySelector("#home_town_edit");
+const currentCityEdit = document.querySelector("#current_city_edit");
+const educationEdit = document.querySelector("#education");
+const edit = document.querySelector("#editButton");
+const editHobbiesButton = document.querySelector("#editButtonHobby");
+const save = document.querySelector("#save");
+const save_hobbies = document.querySelector("#save_hobbies");
+const hobby_1 = document.querySelector("#hobby-1");
+const hobby_2 = document.querySelector("#hobby-2");
+const hobby_3 = document.querySelector("#hobby-3");
+const hobby_4 = document.querySelector("#hobby-4");
 const id = window.sessionStorage.getItem("userId");
+
+edit.addEventListener("click", editButtonAboutMe);
+save.addEventListener("click", editAboutMe);
+save_hobbies.addEventListener("click", saveHobbies);
+editHobbiesButton.addEventListener("click", editHobbies);
 
 db.users.get(parseInt(id)).then((user) => {
   fullName.innerHTML = user.firstName + " " + user.lastName;
@@ -13,6 +40,14 @@ db.users.get(parseInt(id)).then((user) => {
   email.innerHTML = user.email;
   bDate.innerHTML = user.birthDate;
   sex.innerHTML = user.sex;
+  nationality.innerHTML = user.nationality;
+  currentCity.innerHTML = user.currentCity;
+  homeTown.innerHTML = user.homeTown;
+  education.innerHTML = user.edu;
+  hobby_1.innerHTML = user.hobby_1;
+  hobby_2.innerHTML = user.hobby_2;
+  hobby_3.innerHTML = user.hobby_3;
+  hobby_4.innerHTML = user.hobby_4;
   profileSetter(user);
 });
 
